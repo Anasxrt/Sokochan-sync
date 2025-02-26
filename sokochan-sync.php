7d0ee23fb4e4c4982982dbe5701127fd58e7ecaa
Montri Udomariyah <Anasxrt@users.noreply.github.com>
2025-02-27 00:20:09 +0700

    Update sokochan-sync.php
    Enable automatic add sync job, if the job was deleted.

--- sokochan-sync.php
+++ sokochan-sync.php
@@ -2,22 +2,17 @@
 /**
  * Plugin Name: Sokochan sync
  * Description: A plugin to schedule cron jobs for Sokochan API actions.
- * Version: 1.1
+ * Version: 1.1.1
  * Author: Montri Udomariyah
+ * Author URI: https://github.com/Anasxrt/Sokochan-sync
  */
 
 // Hook to schedule the cron jobs on plugin activation
-register_activation_hook(__FILE__, 'sokochan_schedule_cron_jobs');
+register_activation_hook(__FILE__, 'sokochan_check_jobs');
 
-function sokochan_schedule_cron_jobs() {
-    // Schedule the job checker to run every 15 minutes
-    if (!wp_next_scheduled('sokochan_check_and_schedule_jobs')) {
-        wp_schedule_event(time(), 'fifteen_minutes', 'sokochan_check_and_schedule_jobs');
-}
+add_action('init', 'sokochan_check_jobs');
 
 // Hook the function to check and schedule jobs
-add_action('sokochan_check_and_schedule_jobs', 'sokochan_check_jobs');
-
 function sokochan_check_jobs() {
     if (!wp_next_scheduled('sync_stock_with_sokochan')) {
         wp_schedule_event(time(), 'hourly', 'sync_stock_with_sokochan');
